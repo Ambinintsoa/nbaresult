39ac3d68d1292f37015cf863131c7504090ac2a9
Ambinintsoa <randriambololonaisabelle@gmail.com>
2023-11-21 14:34:23 +0300

    Update JoueurService.js

--- src/services/JoueurService.js
+++ src/services/JoueurService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const EMPLOYEE_API_BASE_URL = 'http://localhost:8080/api/v1/employees';
+const EMPLOYEE_API_BASE_URL = 'https://api.publicapis.org/entries';
 class JoueurService {
     getJoueurs(){
         return axios.get(EMPLOYEE_API_BASE_URL);
