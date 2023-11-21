b291768c20e434584ae3914f8832bb5dd28218a5
Ambinintsoa <randriambololonaisabelle@gmail.com>
2023-11-21 14:17:05 +0300

    Update JoueurService.js

--- src/services/JoueurService.js
+++ src/services/JoueurService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const EMPLOYEE_API_BASE_URL = 'https://v2.nba.api-sports.io/players';
+const EMPLOYEE_API_BASE_URL = '';
 class JoueurService {
     getJoueurs(){
         return axios.get(EMPLOYEE_API_BASE_URL);
