8c7cc8d20b32e1a2cac240311a5ccf96acb99402
Ambinintsoa <randriambololonaisabelle@gmail.com>
2023-11-21 13:36:20 +0300

    Delete JoueurService.js

--- src/services/JoueurService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const EMPLOYEE_API_BASE_URL = 'https://v2.nba.api-sports.io/players';
-class JoueurService {
-    getJoueurs(){
-        return axios.get(EMPLOYEE_API_BASE_URL);
-    }
-}
-export default new JoueurService();
